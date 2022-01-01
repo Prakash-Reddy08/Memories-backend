cb2eda6ce21212c96dc497d66d3fe417bbc93b19
Prakash-Reddy08 <mjprakashreddy1@gmail.com>
2022-01-01 18:36:10 +0530

    change

--- index.js
+++ index.js
@@ -12,9 +12,10 @@ require('./auth/passportGoogle');
 const app = express();
 
 app.use(morgan('dev'));
-app.use(helmet())
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(helmet());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.enable('trust proxy');
 app.use(cookieSession({
     maxAge: 24 * 60 * 60 * 1000,
     keys: [process.env.COOKIE_KEY]
